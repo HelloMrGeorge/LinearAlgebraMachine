70c3b1e7c73c18c03fa5b0ac93f06fc186cf0faf
Geo <hellomrgeorge@outlook.com>
2021-10-19 23:50:30 +0800

    2021.10.19 inverse2

---
+++ output/__init__.py
@@ -0,0 +1,4 @@
+import sys
+from pathlib import Path
+lampy_dir = Path(__file__).resolve().parent.parent
+sys.path.append(str(lampy_dir))
---
+++ output/slvdeter.py
@@ -0,0 +1,17 @@
+from lam.determinant.course import det_course
+from lam.readtext.readtext import readtext
+import sympy as sp
+import json
+def slvdeter(a:str):
+    M=det_course(readtext(a))
+    process=M["expand"]
+    result=M["figure"]
+    m=sp.latex(process)
+    n=sp.latex(result)
+    course = {}
+    course['expand'] = m
+    course['figure'] = n
+    json_str = json.dumps(course)
+    with open('test_data.json', 'w') as json_file:
+        json_file.write(json_str)
+
