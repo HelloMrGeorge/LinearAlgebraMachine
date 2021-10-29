e6bdf66e7e144183be2bd159d3ba1b0323f4f219
half-lang <1416001850@qq.com>
2021-10-29 16:55:30 +0800

    更新determinant

---
+++ mysite/determinant/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from output import slvdeter
+from django.http.response import HttpResponse
+from django.core import serializers
+import json
+# Create your views here.
+from django.http import JsonResponse
+def determinant(request):
+    return render(request, 'demo/demoPage.html')
+
+
+def answer(request):
+    print(request)
+    mat = request.POST.get('matrix')
+    print(mat)
+    slvdeter.slvdeter(mat)
+    # 将字符串类型数据转换为json
+    with open('../output/test_data.json', 'r') as json_file:
+        jsondata = json_file.read()
+    return JsonResponse(jsondata, safe=False)
+
