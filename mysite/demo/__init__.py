bb10f0db54c4e9f9aea190d57a3d14d3f507c4e4
Geo <hellomrgeorge@outlook.com>
2021-11-23 21:39:57 +0800

    fix slvdeter.py

--- mysite/demo/__init__.py
+++ mysite/demo/__init__.py
@@ -1,5 +1,2 @@
-import sys
-from pathlib import Path
-lampy_dir = Path(__file__).resolve().parent.parent.parent
-sys.path.append(str(lampy_dir))
+
 
--- mysite/demo/urls.py
+++ mysite/demo/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.demo, name='demo'),
-    path('answer', views.answer, name='demoAnswer'),
-    path('testView', views.testView, name='testView'),
-    path('testAnswer', views.testAnswer, name='testAnswer')
+    # path('', views.demo, name='demo'),
+    # path('answer', views.answer, name='demoAnswer'),
+    # path('testView', views.testView, name='testView'),
+    # path('testAnswer', views.testAnswer, name='testAnswer')
     # path('demo3', views.demo, name='demo3'),
     # path('GEM', views.gaussElim),
     # path('GEMans', views.guassAns, name='GEMans'),
