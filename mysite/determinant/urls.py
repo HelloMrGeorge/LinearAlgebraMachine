992836ef2cab2712af399da38e3ae75224d933f2
George <hellomrgeorge@outlook.com>
2021-11-26 20:36:05 +0800

    repair url

--- mysite/determinant/urls.py
+++ mysite/determinant/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('',views.determinant,name='determinant'),
-    path('deteranswer', views.deteranswer, name='deteranswer'),
     path('answer', views.answer, name='answer'),
     path('test', views.test, name='csrf_test'),
     path('csrf', views.csrftest),
