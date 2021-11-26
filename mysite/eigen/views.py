226844ccbe55aa8505c87397ba72a1206722210e
half-lang <1416001850@qq.com>
2021-11-26 08:40:38 +0800

    修改view的传输格式

--- mysite/eigen/views.py
+++ mysite/eigen/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import JsonResponse
 from output import slvdeter
+import logging
+logging.basicConfig(level=logging.DEBUG,filename='mylog.txt',filemode='w')
 def eigen(request):
     return render(request, 'demo/eigenPage.html')
 def eigenanswer(request):
     mat = request.POST.get('matrix')
     jsondata=slvdeter.slvdeter_eigen(mat)
-    print(type(jsondata))
+    logging.debug(jsondata)
+    logging.debug(type(jsondata))
     return JsonResponse(jsondata, safe=False)
 
