7f795878e6a1900c9d6518e01ae1834e25f048b2
George <hellomrgeorge@outlook.com>
2021-11-27 17:40:28 +0800

    update quad module

--- lam/quad/quadbase.py
+++ lam/quad/quadbase.py
@@ -1,10 +1,10 @@
 import logging
 import sympy as sp
-from sympy.matrices.dense import MutableDenseMatrix
+from sympy.matrices.dense import MutableDenseMatrix, eye
 
 logging.basicConfig(level=logging.DEBUG)
 
-def is_square(mat: MutableDenseMatrix, i: int) -> bool:
+def is_complete_square(mat: MutableDenseMatrix, i: int) -> bool:
     #判断平方项a_{ii}是否已经配方
     flag = True
     for k in range(mat.shape[0]):
@@ -12,6 +12,7 @@ def is_square(mat: MutableDenseMatrix, i: int) -> bool:
             flag = False
             break
     return flag
+    
 
 def quad_term_trans(mat: MutableDenseMatrix, i: int) -> MutableDenseMatrix:
     # 配方平方项的线性变数替换，i表示平方项的下标
@@ -30,11 +31,34 @@ def cross_term_trans(mat: MutableDenseMatrix, i: int , j:int) -> MutableDenseMat
     ret[i,i], ret[j,j], ret[i,j], ret[j,i] = 1, -1, 1, 1
     return ret
 
+def next_quad_op(mat: MutableDenseMatrix) -> MutableDenseMatrix:
+    #找到对mat进行下一步标准化操作的变换
+    if mat.is_symmetric() == False:
+        raise Exception('不是对称矩阵')
+
+    dim: int = mat.shape[0]
+    for i in range(dim):
+        if is_complete_square(mat, i) == False and mat[i,i] != 0:
+            return quad_term_trans(mat, i)
+    
+    for i in range(dim):
+        for j in range(dim):
+            if mat[i,j] != 0 and i != j:
+                return cross_term_trans(mat, i, j)
+    
+    return eye(dim)
+
 if __name__ == "__main__":
     mat = sp.Matrix([
         [1,3,4],
         [3,2,3],
         [4,3,7],
     ])
-    T = cross_term_trans(mat, 0,1)
-    logging.debug(T)
+    tra = next_quad_op(mat)
+    logging.debug(tra)
+    mat = tra.T @ mat @ tra
+    logging.debug(mat)
+    tra = next_quad_op(mat)
+    logging.debug(tra)
+    mat = tra.T @ mat @ tra
+    logging.debug(mat)
