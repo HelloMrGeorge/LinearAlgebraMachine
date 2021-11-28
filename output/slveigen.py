ef1ee8832481054903a36a7f2bd73c88ed5e7a88
half-lang <1416001850@qq.com>
2021-11-28 23:00:41 +0800

    更改eigen

--- output/slveigen.py
+++ output/slveigen.py
@@ -20,7 +20,6 @@ def slveigengetcharpoly(a:str):#特征多项式
     eigenSolver = EigenSolver(readtext(a))
     p = eigenSolver.getCharpoly()
     p=sp.latex(p)
-    print(p)
     json_str = json.dumps(p)
     return json_str
 def slveigenvalue(a:str):#特征值
@@ -33,8 +32,12 @@ def slveigenvectors(a:str):#特征向量
     eigenSolver = EigenSolver(readtext(a))
     eigenvectors = eigenSolver.getEigenvectors()
     eigenvectors_0 = []
+    print(eigenvectors[0][2])
     for i in range(len(eigenvectors)):
-        m = (sp.latex(eigenvectors[i][0]), sp.latex(eigenvectors[i][1]), sp.latex(eigenvectors[i][2][0]))
+        p = []
+        for j in range(len(eigenvectors[i][2])):
+            p.append(sp.latex(eigenvectors[i][2][j]))
+        m = (sp.latex(eigenvectors[i][0]), sp.latex(eigenvectors[i][1]), sp.latex(p))
         eigenvectors_0.append(m)
     eigenvectors=eigenvectors_0
     json_str = json.dumps(eigenvectors)
@@ -49,7 +52,10 @@ def slveigenCourse(a:str):#（特征值求解过程）
     charpoly=sp.latex(charpoly)
     eigenvectors_0=[]
     for i in range(len(eigenvectors)):
-        m = (sp.latex(eigenvectors[i][0]),sp.latex(eigenvectors[i][1]),sp.latex(eigenvectors[i][2][0]))
+        q = []
+        for j in range(len(eigenvectors[i][2])):
+            q.append(sp.latex(eigenvectors[i][2][j]))
+        m = (sp.latex(eigenvectors[i][0]), sp.latex(eigenvectors[i][1]), sp.latex(q))
         eigenvectors_0.append(m)
     p['matrix']=matrix
     p['eigenvetors']=eigenvectors_0
@@ -57,3 +63,4 @@ def slveigenCourse(a:str):#（特征值求解过程）
     json_str = json.dumps(p,cls=MyEncoder,indent=4)
     return json_str
 
+slveigenCourse("[[1,2,3],[5,62,2],[245,4,6]]")
