7db0e2dcd4a0c1e9abd13cccd8c0020cd06efb2f
Geo <hellomrgeorge@outlook.com>
2021-11-30 22:09:36 +0800

    test eigen

--- mysite/determinant/views.py
+++ mysite/determinant/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from output import slvdeter
-from django.http.response import HttpResponse
 from django.http import JsonResponse
 import json
 
--- mysite/eigen/views.py
+++ mysite/eigen/views.py
@@ -1,33 +1,35 @@
 from django.shortcuts import render
-# Create your views here.
 from django.http import JsonResponse
 from output import slveigen
-import logging
 import json
+
+import logging
 logging.basicConfig(level=logging.DEBUG,filename='mylog.txt',filemode='w')
+
 def eigen(request):
     return render(request, 'demo/eigenPage.html')
+
 def eigenCourse(request):
     mat = json.loads(request.body)['matrix']
-    jsondata=slveigen.slveigenvectorsCourse(mat)
+    jsondata=slveigen.slveigenCourse(mat)
     logging.debug(jsondata)
-    logging.debug(type(jsondata))
     return JsonResponse(jsondata, safe=False)
+
 def eigenvectors(request):
     mat = json.loads(request.body)['matrix']
     jsondata=slveigen.slveigenvectors(mat)
-    logging.debug(jsondata)
-    logging.debug(type(jsondata))
+
     return JsonResponse(jsondata, safe=False)
+
 def eigenvalue(request):
     mat = json.loads(request.body)['matrix']
     jsondata=slveigen.slveigenvalue(mat)
-    logging.debug(jsondata)
-    logging.debug(type(jsondata))
+
     return JsonResponse(jsondata, safe=False)
+
 def eigencharpoly(request):
     mat = json.loads(request.body)['matrix']
     jsondata=slveigen.slveigengetcharpoly(mat)
-    logging.debug(jsondata)
-    logging.debug(type(jsondata))
+
     return JsonResponse(jsondata, safe=False)
+
